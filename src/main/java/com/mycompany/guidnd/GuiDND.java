bf09eb6b34f05a6d90252fa6c1462af4540937e0
Madhav Asok <masok@echo360.com>
2023-04-11 12:38:32 -0700

    Initial Code Commit
    Adding base project code

---
+++ src/main/java/com/mycompany/guidnd/GuiDND.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guidnd;
+
+/**
+ *
+ * @author madhavasok
+ */
+public class GuiDND {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
